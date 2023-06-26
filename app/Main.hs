58d3062c3a25e97cafb07b207187addc4ef04a32
siavava <amittaijoel@outlook.com>
2023-06-26 17:47:29 -0400

    #5

--- app/Main.hs
+++ app/Main.hs
@@ -15,13 +15,25 @@ import System.Console.ANSI(
 import Data.Traversable (forM)
 import Control.Monad (forM_)
 
+-- | given a number, split it into 50 digits per line.
+prettyFormat :: Integer -> String
+prettyFormat n = iter (show n) ""
+  where
+    iter :: String -> String -> String
+    iter s acc
+      | length s <= 50    = printf "%s\n%s" s acc
+      | otherwise         = printf "%s\n%s" (take 50 s) (iter (drop 50 s) acc)
 
 
 -- | Print provided number in Green.
-colorize :: Show a => Int -> a -> IO ()
+colorize :: (PrintfArg a) => Int -> a -> IO ()
 colorize id solution = do
   setSGR [SetColor Foreground Vivid Blue]
-  printf "\n\n\t  #%d: %s\t\n" id $ show solution
+  printf "\n\n--------------------------------------------------\n"
+  printf "#Problem %d\n" id
+  printf "--------------------------------------------------\n"
+  printf "%s" solution
+  printf "--------------------------------------------------\n"
   setSGR [Reset]
 
 
@@ -29,7 +41,7 @@ colorize id solution = do
 main :: IO ()
 main = do
   forM_ (enumerate P.solutions) $ \(i, solution) -> do
-    colorize i solution
+    colorize i $ prettyFormat solution
 
 enumerate :: [a] -> [(Int, a)]
 enumerate = zip [1..]
