394ef476c1a91e0b638f8ed697b526fb43160b0d
siavava <amittaijoel@outlook.com>
2023-07-03 00:22:39 -0400

    rs1

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,16 +1,24 @@
 
 use num::bigint::BigInt;
 
-/**
- * Problem 1: Find sum of multiples of values in a set below a limit.
- */
+///
+/// Project Euler - Problem 1:
+/// Find the sum all all multiples of 3 or 5 below 1000.
+/// 
+/// # Example
+/// ```
+/// use num::BigInt;
+/// use problems::problem1::solve;
+/// 
+/// assert_eq!(solve(vec![3,5], 10), BigInt::from(23));
+/// assert_eq!(solve(vec![3,5], 1000), BigInt::from(233168));
+/// ```
 pub fn solve(divisors: Vec<i32>, limit: i32) -> BigInt {
-  let mut sum: BigInt = BigInt::from(0);
 
   // largest bigint possible
   let mut sum: BigInt = BigInt::from(0);
 
-  for i in 1..1000 {
+  for i in 1..limit {
     for divisor in &divisors {
       if i % divisor == 0 {
         sum += i;
